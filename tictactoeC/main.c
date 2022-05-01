79f8cd0f780799609df0cc7e3af567b16dc0f687
Tasneemhasanat97 <tasneem.hasanat01@qmail.cuny.edu>
2022-05-01 00:49:54 -0400

    Initial Commit

---
+++ tictactoeC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  tictactoeC
+//
+//  Created by Tasneem Hasanat on 5/1/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
